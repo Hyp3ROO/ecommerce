96157710d11a853fba321441eb7a0b4bd38625ee
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-02-19 14:12:29 +0100

    added functions for signing in and out

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,22 @@
+import { useAuthState } from 'react-firebase-hooks/auth'
 import { useState } from 'react'
 import { AiFillHome } from 'react-icons/ai'
 import { BsBasket3Fill } from 'react-icons/bs'
 import Hamburger from 'hamburger-react'
+import { auth } from '../auth/firebase'
+import { signOutUser } from '../auth/auth'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
+  const [user] = useAuthState(auth)
   const [isOpen, setIsOpen] = useState(false)
+
+  const handleSignOut = () => {
+    signOutUser()
+    if (isOpen) {
+      setIsOpen(false)
+    }
+  }
 
   return (
     <>
@@ -18,19 +30,33 @@ const NavBar = () => {
           className={`${
             isOpen ? 'fixed' : 'hidden'
           } inset-0 flex min-h-screen flex-col items-center justify-around bg-white/80 backdrop-blur-sm`}>
-          <li className='group flex items-center justify-center p-4 text-xl duration-200 hover:scale-110 hover:text-blue-700'>
-            <AiFillHome className='mr-3 text-blue-500 duration-200 group-hover:scale-110 group-hover:text-blue-700' />
-            Home
-          </li>
+          <Link to='/'>
+            <li className='group flex items-center justify-center p-4 text-xl duration-200 hover:scale-110 hover:text-blue-700'>
+              <AiFillHome className='mr-3 text-blue-500 duration-200 group-hover:scale-110 group-hover:text-blue-700' />
+              Home
+            </li>
+          </Link>
           <li className='group flex items-center justify-center p-4 text-xl duration-200 hover:scale-110 hover:text-blue-700'>
             <BsBasket3Fill className='mr-3 text-xl text-blue-500 duration-200 group-hover:scale-110 group-hover:text-blue-700 md:text-2xl' />
             Basket
           </li>
-          <li>
-            <button className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'>
-              Log In
-            </button>
-          </li>
+          {!user ? (
+            <li>
+              <Link to='/sign-in'>
+                <button className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'>
+                  Sign In
+                </button>
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <button
+                className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'
+                onClick={handleSignOut}>
+                Sign Out
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
 
@@ -48,11 +74,23 @@ const NavBar = () => {
               <BsBasket3Fill className='text-xl text-blue-500 duration-200 group-hover:scale-110 group-hover:text-blue-700 md:text-2xl' />
             </button>
           </li>
-          <li>
-            <button className='rounded-lg border border-blue-700 px-3 py-1.5 text-center text-sm text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800 md:px-4 md:py-2 lg:px-6 lg:py-2.5'>
-              Log In
-            </button>
-          </li>
+          {!user ? (
+            <li>
+              <Link to='/sign-in'>
+                <button className='rounded-lg border border-blue-700 px-3 py-1.5 text-center text-sm text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800 md:px-4 md:py-2 lg:px-6 lg:py-2.5'>
+                  Sign In
+                </button>
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <button
+                className='rounded-lg border border-blue-700 px-3 py-1.5 text-center text-sm text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800 md:px-4 md:py-2 lg:px-6 lg:py-2.5'
+                onClick={handleSignOut}>
+                Sign Out
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </>
