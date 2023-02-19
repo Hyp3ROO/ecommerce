bb6e85ac64371ae5a8d7bc54da329f0ec93c1a76
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-02-19 20:11:11 +0100

    updated firebase config

--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth'
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_SOME_API_KEY,
-  authDomain: import.meta.env.VITE_SOME_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_SOME_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_SOME_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_SOME_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_SOME_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 }
 
 // Initialize Firebase
