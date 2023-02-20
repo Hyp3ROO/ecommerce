99fb42f8a3dd535197d64ee293103b84023731e4
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-02-20 10:39:03 +0100

    added Loading component before fully loaded image

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -1,15 +1,27 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css'
+import ReactLoading from 'react-loading'
 
 const ProductImage = ({ image, alt }: any) => {
   console.log(image, alt)
   return (
-    <LazyLoadImage
-      alt={alt}
-      src={image}
-      effect='blur'
-      className='h-[20rem] object-contain'
-    />
+    <div className='relative'>
+      <LazyLoadImage
+        alt={alt}
+        src={image}
+        effect='blur'
+        beforeLoad={() => (
+          <ReactLoading
+            type='bars'
+            width={'20%'}
+            height={'20%'}
+            color='rgb(59 130 246)'
+            className='absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2'
+          />
+        )}
+        className='h-[20rem] object-contain'
+      />
+    </div>
   )
 }
 export default ProductImage
