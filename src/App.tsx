a9acf2f2afac90bf777e72aa2ba573e8e9eb759c
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-02-21 21:47:55 +0100

    added some functions for Cart handling and updated Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
 import { useEffect, useState } from 'react'
 import { fetchFromApi } from './api/api'
+import { Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import AllProductsList from './components/AllProductsList'
-import { Route, Routes } from 'react-router-dom'
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage'
+import CartPage from './pages/CartPage'
+import { Product } from './types/Product'
 
 const App = () => {
-  const [products, setProducts] = useState([])
-  const [categories, setCategories] = useState([])
+  const [products, setProducts] = useState<Product[]>([])
+  const [cartItems, setCartItems] = useState<Product[]>(() => {
+    return JSON.parse(localStorage.getItem('cartItems') || '[]')
+  })
+
+  const addProductToCart = (product: Product) => {
+    setCartItems([...cartItems, product])
+    localStorage.setItem('cartItems', JSON.stringify([...cartItems, product]))
+  }
+
+  const deleteProductFromCart = (id: number) => {
+    const updatedCartItems = cartItems.filter(
+      (cartItem: { id: number }) => cartItem.id !== id
+    )
+    setCartItems(updatedCartItems)
+    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems))
+  }
 
   const fetchAllProducts = async () => {
     const response = await fetchFromApi('products')
     setProducts(response)
   }
 
-  const fetchAllCategories = async () => {
-    const response = await fetchFromApi('products/categories')
-    setCategories(response)
-  }
-
   useEffect(() => {
     fetchAllProducts()
-    fetchAllCategories()
   }, [])
 
   return (
@@ -36,7 +47,26 @@ const App = () => {
                 <NavBar />
               </header>
               <main className='p-12'>
-                <AllProductsList products={products} />
+                <AllProductsList
+                  products={products}
+                  addProductToCart={addProductToCart}
+                />
+              </main>
+            </>
+          }
+        />
+        <Route
+          path='/cart'
+          element={
+            <>
+              <header className='h-20'>
+                <NavBar />
+              </header>
+              <main className='p-12'>
+                <CartPage
+                  cartItems={cartItems}
+                  deleteProductFromCart={deleteProductFromCart}
+                />
               </main>
             </>
           }
