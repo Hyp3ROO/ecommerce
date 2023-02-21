2df5cd2f926ea583846a0b715920d30a4f0c0703
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-02-21 21:49:13 +0100

    added types, Cart handling and Toast notifications

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,31 +1,44 @@
 import ProductImage from './ProductImage'
+import { Product } from '../types/Product'
+import toast, { Toaster } from 'react-hot-toast'
 
 type ProductProps = {
-  product: {
-    id: number
-    title: string
-    category: string
-    description: string
-    image: string
-    price: number
-    rating: {
-      count: number
-      rate: number
-    }
-  }
+  product: Product
+  addProductToCart: (product: Product) => void
 }
 
-const ProductCard = ({ product }: ProductProps) => {
+const notify = () => toast('Successfully added item to cart')
+
+const ProductCard = ({ product, addProductToCart }: ProductProps) => {
+  const handleClick = () => {
+    notify()
+    addProductToCart(product)
+  }
+
   return (
     <div className='flex flex-col items-center justify-center py-6'>
-      <ProductImage image={product?.image} alt={product?.title} />
+      <ProductImage
+        image={product?.image}
+        alt={product?.title}
+        className='h-[20rem] object-contain'
+      />
       <div className='flex flex-col items-center text-center'>
         <h3 className='my-4 font-bold'>{product?.title}</h3>
         <p className='my-2 font-bold'>{`${product?.price}$`}</p>
-        <button className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'>
-          Buy Now
+        <button
+          className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'
+          onClick={handleClick}>
+          Add to Cart
         </button>
       </div>
+      <Toaster
+        toastOptions={{
+          style: {
+            background: 'rgb(59 130 246)',
+          },
+          duration: 800,
+        }}
+      />
     </div>
   )
 }
