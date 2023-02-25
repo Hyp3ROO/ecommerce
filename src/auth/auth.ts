2962601469d80e1e20913d8866d8dc5652d5243d
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-02-25 20:43:03 +0100

    added Toast notifications

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -4,31 +4,34 @@ import {
   createUserWithEmailAndPassword,
 } from 'firebase/auth'
 import { auth } from './firebase'
+import toast from 'react-hot-toast'
+
+const notify = (text: string, color: string) =>
+  toast(text, {
+    style: { color },
+  })
 
 const createUser = (email: string, password: string) => {
   createUserWithEmailAndPassword(auth, email, password)
-    .then()
-    .catch(error => {
-      const errorMessage = error.message
-      alert(errorMessage)
+    .then(() => notify('Created user', 'green'))
+    .catch(() => {
+      notify('Unable to create user. Try again!', 'red')
     })
 }
 
 const signInUser = (email: string, password: string) => {
   signInWithEmailAndPassword(auth, email, password)
-    .then()
-    .catch(error => {
-      const errorMessage = error.message
-      alert(errorMessage)
+    .then(() => notify('Signed in', 'green'))
+    .catch(() => {
+      notify('Unable to sign in. Try again!', 'red')
     })
 }
 
 const signOutUser = () => {
   signOut(auth)
-    .then()
-    .catch(error => {
-      const errorMessage = error.message
-      alert(errorMessage)
+    .then(() => notify('Signed out', 'red'))
+    .catch(() => {
+      notify('Unable to sign out. Try again!', 'red')
     })
 }
 
