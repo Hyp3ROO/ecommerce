e3f2338963f5de5e04fca590774f9ae5540aa43f
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-02-26 16:39:39 +0100

    added total price in the cart

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,6 @@
 import { Product } from '../types/Product'
 import CartItem from '../components/CartItem'
+import { useEffect, useState } from 'react'
 
 type CartPageProps = {
   cartItems: Product[]
@@ -12,6 +13,7 @@ const CartPage = ({
   deleteProductFromCart,
   handleQuantityChange,
 }: CartPageProps) => {
+  const [total, setTotal] = useState(0)
   const renderedCartItems = cartItems?.map((cartItem: Product) => (
     <CartItem
       key={cartItem.id}
@@ -21,6 +23,14 @@ const CartPage = ({
     />
   ))
 
+  useEffect(() => {
+    let totalPrice = 0
+    cartItems.forEach(cartItem => {
+      totalPrice += cartItem.price * cartItem.quantity
+    })
+    setTotal(Math.round(totalPrice * 100) / 100)
+  }, [cartItems])
+
   return (
     <div className='relative'>
       {cartItems.length > 0 ? (
@@ -29,8 +39,14 @@ const CartPage = ({
             <h2 className='mb-8 text-2xl font-bold md:text-3xl'>Cart:</h2>
             <div className='grid gap-12'>{renderedCartItems}</div>
           </div>
-          <div>
-            <h2 className='text-2xl font-bold md:text-3xl'>Subtotal:</h2>
+          <div className='flex flex-col gap-4'>
+            <div className='flex items-center gap-2'>
+              <h2 className='text-2xl font-bold md:text-3xl'>Total:</h2>
+              <p className='text-xl md:text-2xl'>{total}$</p>
+            </div>
+            <button className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'>
+              Checkout
+            </button>
           </div>
         </div>
       ) : (
