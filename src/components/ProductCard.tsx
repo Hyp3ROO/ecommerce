f96a7a50680b079bf5b994e763f705a5e54bb53e
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-02-27 23:27:00 +0100

    added Rating

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import ProductImage from './ProductImage'
 import { Product } from '../types/Product'
+import Rating from '@mui/material/Rating'
 
 type ProductProps = {
   product: Product
@@ -20,6 +21,15 @@ const ProductCard = ({ product, addProductToCart }: ProductProps) => {
       />
       <div className='flex flex-col items-center text-center'>
         <h3 className='my-4 font-bold'>{product?.title}</h3>
+        <p className='flex items-center justify-center gap-1 text-sm font-bold'>
+          <Rating
+            name='read-only'
+            value={product?.rating.rate}
+            precision={0.1}
+            readOnly
+          />
+          ({product?.rating.count})
+        </p>
         <p className='my-2 font-bold'>{`${product?.price}$`}</p>
         <button
           className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'
