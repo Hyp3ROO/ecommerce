b430db99449be9e4bcf09ee84930d3a09f25ff31
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-04-11 14:26:05 +0200

    updated types

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -2,8 +2,8 @@ import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 
 type ProductImageProps = {
-  image: string
-  alt: string
+  image: string | undefined
+  alt: string | undefined
   [rest: string]: any
 }
 
