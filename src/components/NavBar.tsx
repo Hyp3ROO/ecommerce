340ddaa77b98ca113514864743f1b0bab4fa59c4
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-04-11 14:19:27 +0200

    added dark mode and changed some styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,25 @@
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { AiFillHome } from 'react-icons/ai'
-import { BsBasket3Fill } from 'react-icons/bs'
+import { GoTasklist } from 'react-icons/go'
+import { ImCart } from 'react-icons/im'
 import Hamburger from 'hamburger-react'
 import { auth } from '../auth/firebase'
 import { signOutUser } from '../auth/auth'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Product } from '../types/Product'
 import Tooltip from '@mui/material/Tooltip'
+import { BsMoon, BsSun } from 'react-icons/bs'
 
 type NavBarProps = {
   cartItems: Product[]
 }
 
 const NavBar = ({ cartItems }: NavBarProps) => {
+  const location = useLocation()
   const [user] = useAuthState(auth)
   const [isOpen, setIsOpen] = useState(false)
+  const [theme, setTheme] = useState(localStorage.theme || 'light')
 
   const handleSignOut = () => {
     signOutUser()
@@ -24,18 +28,44 @@ const NavBar = ({ cartItems }: NavBarProps) => {
     }
   }
 
+  const handleTheme = () => {
+    theme === 'dark' ? setTheme('light') : setTheme('dark')
+  }
+
+  useEffect(() => {
+    switch (theme) {
+      case 'dark':
+        document.body.classList.add('dark')
+        localStorage.theme = 'dark'
+        break
+      case 'light':
+        document.body.classList.remove('dark')
+        localStorage.theme = ''
+        break
+    }
+  }, [theme])
+
   return (
     <>
       {/* Mobile NavBar */}
-      <nav className='fixed inset-y-0 inset-x-12 z-10 flex h-20 items-center justify-between bg-white/60 backdrop-blur-sm md:hidden'>
-        <h1 className='text-lg font-bold'>E-COM</h1>
+      <nav
+        className={`fixed inset-y-0 z-10 flex h-20 w-full items-center justify-between bg-white/80 px-12 dark:bg-gray-900/80 md:hidden ${
+          (location.pathname === '/sign-in' ||
+            location.pathname === '/sign-up') &&
+          'hidden'
+        }`}>
+        <Link to='/'>
+          <h1 className='text-lg font-bold duration-200 hover:text-blue-500'>
+            E-COM
+          </h1>
+        </Link>
         <div className='z-10'>
           <Hamburger duration={0.2} toggled={isOpen} toggle={setIsOpen} />
         </div>
         <ul
           className={`${
             isOpen ? 'fixed' : 'hidden'
-          } inset-0 flex min-h-screen flex-col items-center justify-evenly bg-white/80 backdrop-blur-sm`}>
+          } inset-0 flex min-h-screen flex-col items-center justify-evenly bg-white/80 dark:bg-gray-900/80`}>
           <Link to='/' onClick={() => setIsOpen(false)}>
             <li className='group flex items-center justify-center p-4 text-xl duration-200 hover:scale-110 hover:text-blue-700'>
               <AiFillHome className='mr-3 text-blue-500 duration-200 group-hover:text-blue-700' />
@@ -44,13 +74,30 @@ const NavBar = ({ cartItems }: NavBarProps) => {
           </Link>
           <Link to='/cart' onClick={() => setIsOpen(false)}>
             <li className='group relative flex items-center justify-center p-4 text-xl duration-200 hover:scale-110 hover:text-blue-700'>
-              <BsBasket3Fill className='mr-3 text-xl text-blue-500 duration-200 group-hover:text-blue-700 md:text-2xl' />
+              <ImCart className='mr-3 text-xl text-blue-500 duration-200 group-hover:text-blue-700 md:text-2xl' />
               Cart
               <span className='absolute top-0.5 -right-1 grid h-5 w-5 place-items-center rounded-full bg-blue-500 text-sm text-black duration-200'>
                 {cartItems.length}
               </span>
             </li>
           </Link>
+          <li>
+            <p
+              className='flex cursor-pointer items-center justify-center gap-1 text-sm font-bold text-blue-500 duration-200 hover:text-blue-700'
+              onClick={handleTheme}>
+              {theme === 'light' ? (
+                <>
+                  <BsMoon className='mr-1 h-5 w-5 md:h-6 md:w-6' />
+                  Dark Mode
+                </>
+              ) : (
+                <>
+                  <BsSun className='mr-1 h-5 w-5 md:h-6 md:w-6' />
+                  Light Mode
+                </>
+              )}
+            </p>
+          </li>
           {!user ? (
             <li>
               <Link to='/sign-in'>
@@ -60,41 +107,73 @@ const NavBar = ({ cartItems }: NavBarProps) => {
               </Link>
             </li>
           ) : (
-            <li>
-              <button
-                className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'
-                onClick={handleSignOut}>
-                Sign Out
-              </button>
-            </li>
+            <>
+              <li>
+                <Link
+                  to='/orders'
+                  className='group relative flex items-center justify-center p-4 text-xl duration-200 hover:scale-110 hover:text-blue-700'>
+                  <GoTasklist className='mr-3 text-xl text-blue-500 duration-200 group-hover:text-blue-700 md:text-2xl' />
+                  Orders
+                </Link>
+              </li>
+              <li>
+                <button
+                  className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'
+                  onClick={handleSignOut}>
+                  Sign Out
+                </button>
+              </li>
+            </>
           )}
         </ul>
       </nav>
 
       {/* Desktop NavBar */}
-      <nav className='fixed inset-y-0 inset-x-12 z-10 hidden h-20 items-center justify-between bg-white/60 backdrop-blur-sm md:flex'>
-        <h1 className='text-lg font-bold md:text-2xl'>E-COM</h1>
-        <ul className='flex items-center justify-center gap-6'>
-          <li>
+      <nav
+        className={`fixed inset-y-0 inset-x-12 z-10 hidden h-20 items-center justify-between bg-white/80 dark:bg-gray-900/80 md:flex ${
+          (location.pathname === '/sign-in' ||
+            location.pathname === '/sign-up') &&
+          'md:hidden'
+        }`}>
+        <Link to='/'>
+          <h1 className='text-lg font-bold duration-200 hover:text-blue-500 md:text-2xl'>
+            E-COM
+          </h1>
+        </Link>
+        <ul className='flex items-center justify-center gap-8'>
+          <li className='grid place-items-center'>
             <Tooltip title='Home' arrow disableInteractive>
               <Link to='/'>
-                <button className='group p-2'>
-                  <AiFillHome className='text-xl text-blue-500 duration-200 group-hover:text-blue-700 md:text-2xl' />
-                </button>
+                <AiFillHome className='text-xl text-blue-500 duration-200 group-hover:text-blue-700 md:text-2xl' />
+              </Link>
+            </Tooltip>
+          </li>
+          <li className='relative grid place-items-center'>
+            <Tooltip title='Cart' arrow disableInteractive>
+              <Link to='/cart'>
+                <ImCart className='text-xl text-blue-500 duration-200 group-hover:text-blue-700 md:text-2xl' />
+                <span className='absolute -top-3 -right-3 grid h-5 w-5 place-items-center rounded-full bg-blue-500 text-sm duration-200'>
+                  {cartItems.length}
+                </span>
               </Link>
             </Tooltip>
           </li>
           <li>
-            <Tooltip title='Cart' arrow disableInteractive>
-              <Link to='/cart'>
-                <button className='group relative p-2'>
-                  <BsBasket3Fill className='text-xl text-blue-500 duration-200 group-hover:text-blue-700 md:text-2xl' />
-                  <span className='absolute -top-1 -right-2 grid h-5 w-5 place-items-center rounded-full bg-blue-500 text-sm duration-200'>
-                    {cartItems.length}
-                  </span>
-                </button>
-              </Link>
-            </Tooltip>
+            <p
+              className='flex cursor-pointer items-center justify-center gap-1 text-sm font-bold text-black duration-200 hover:text-blue-500 dark:text-white dark:hover:text-blue-500'
+              onClick={handleTheme}>
+              {theme === 'light' ? (
+                <>
+                  <BsMoon className='mr-1 h-5 w-5 md:h-6 md:w-6' />
+                  Dark Mode
+                </>
+              ) : (
+                <>
+                  <BsSun className='mr-1 h-5 w-5 md:h-6 md:w-6' />
+                  Light Mode
+                </>
+              )}
+            </p>
           </li>
           {!user ? (
             <li>
@@ -105,13 +184,22 @@ const NavBar = ({ cartItems }: NavBarProps) => {
               </Link>
             </li>
           ) : (
-            <li>
-              <button
-                className='rounded-lg border border-blue-700 px-3 py-1.5 text-center text-sm text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800 md:px-4 md:py-2 lg:px-6 lg:py-2.5'
-                onClick={handleSignOut}>
-                Sign Out
-              </button>
-            </li>
+            <>
+              <li className='grid place-items-center'>
+                <Tooltip title='Orders' arrow disableInteractive>
+                  <Link to='/orders'>
+                    <GoTasklist className='text-xl text-blue-500 duration-200 group-hover:text-blue-700 md:text-2xl' />
+                  </Link>
+                </Tooltip>
+              </li>
+              <li>
+                <button
+                  className='rounded-lg border border-blue-700 px-3 py-1.5 text-center text-sm text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800 md:px-4 md:py-2 lg:px-6 lg:py-2.5'
+                  onClick={handleSignOut}>
+                  Sign Out
+                </button>
+              </li>
+            </>
           )}
         </ul>
       </nav>
