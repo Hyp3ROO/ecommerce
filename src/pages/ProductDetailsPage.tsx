3294a1b6a515fd8f07296d26a93f274a8bcb9fed
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-04-12 19:48:39 +0200

    changed some styling

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -44,14 +44,14 @@ const ProductDetailsPage = ({ addProductToCart }: ProductDetailsProps) => {
           className='top-30 absolute left-[50%] -translate-x-1/2'
         />
       ) : (
-        <div className='grid grid-flow-row place-items-center gap-8 text-center'>
+        <div className='grid grid-flow-row place-items-center gap-8 px-4 text-center md:mt-8 md:grid-cols-2 md:gap-12 md:px-16 lg:px-32'>
           <ProductImage
             image={productDetails?.image}
             alt={productDetails?.title}
-            className='h-[12rem] rounded-lg bg-white object-contain p-4 md:h-[20rem]'
+            className='h-[12rem] rounded-lg bg-white object-contain p-4 md:col-start-1 md:col-end-3 md:h-[20rem]'
           />
           <div className='grid grid-flow-row place-items-center'>
-            <h1 className='font-bold'>{productDetails?.title}</h1>
+            <h1 className='font-bold md:text-2xl'>{productDetails?.title}</h1>
             <p className='my-2 flex items-center justify-start gap-1 font-bold'>
               <Rating
                 name='read-only'
@@ -61,7 +61,7 @@ const ProductDetailsPage = ({ addProductToCart }: ProductDetailsProps) => {
               />
               ({productDetails?.rating.count})
             </p>
-            <p className='my-2 text-lg font-bold'>{`${productDetails?.price}$`}</p>
+            <p className='my-2 text-lg font-bold md:text-xl'>{`${productDetails?.price}$`}</p>
             <p className='mb-6 text-sm'>{productDetails?.description}</p>
             <button
               className='rounded-lg border border-blue-700 px-10 py-2.5 text-center text-lg text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'
