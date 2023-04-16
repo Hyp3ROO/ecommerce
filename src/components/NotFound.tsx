d19cf8cdc92b453b2d536a87ce16b2d63431b7da
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-04-16 15:31:38 +0200

    adapted the photo to smaller devices

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -6,7 +6,11 @@ const NotFound = () => {
 
   return (
     <div className='flex flex-col items-center justify-center gap-4'>
-      <img src={notFoundImg} alt='Error 404' className='h-[30rem]' />
+      <img
+        src={notFoundImg}
+        alt='Error 404'
+        className='h-[20rem] md:h-[30rem]'
+      />
       <button
         className='rounded-lg border border-blue-700 px-3 py-1.5 text-center text-sm text-blue-700 duration-200 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800 md:px-4 md:py-2 lg:px-6 lg:py-2.5'
         onClick={() => navigate('/')}>
