b8c070a96985c5ad94e93fe2043c3674c52140e2
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-04-16 14:51:22 +0200

    deleted getOneProduct and simplified getProducts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,26 +1,11 @@
 import axios, { AxiosError } from 'axios'
-import { Product } from '../types/Product'
 
 const API_URL = 'https://fakestoreapi.com/products'
 
 const getProducts = async () => {
   try {
     const response = await axios.get(API_URL)
-    const updatedResponse = response.data.map((item: Product) => {
-      return { ...item, quantity: 0 }
-    })
-    return updatedResponse
-  } catch (error) {
-    const err = error as AxiosError
-    throw new Error(err.message)
-  }
-}
-
-const getOneProduct = async (id: string) => {
-  try {
-    const response = await axios.get(`${API_URL}/${id}`)
-    const updatedResponse = { ...response.data, quantity: 0 }
-    return updatedResponse
+    return response.data
   } catch (error) {
     const err = error as AxiosError
     throw new Error(err.message)
@@ -37,4 +22,4 @@ const getCategories = async () => {
   }
 }
 
-export { getProducts, getOneProduct, getCategories }
+export { getProducts, getCategories }
